4c0eb669a6bb8fe11f59004f00ed17b99fa0b5f5
Tanishq K <kronorak@outlook.com>
2021-02-18 13:21:12 +0530

    Update repeatString.js
    Updating the code in order to pass the 5th test as specified in the spec
    file

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(stringInput, repeatTimes) {
+    let str = "";
 
+    if(repeatTimes < 0) {
+        return "ERROR"
+    }
 }
 
 module.exports = repeatString
