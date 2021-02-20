1496d37e4376c3e39e88520707c87bb42950f6ba
Tanishq K <kronorak@outlook.com>
2021-02-20 09:42:41 +0530

    Update reverseString
       - Implemented the logic to reverse the string
       - Enabled the temporarily disabled tests in the spec file

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let revStr = "";
 
+    for(let i = str.length - 1; i >=0; i--) {
+        revStr += str[i];
+    }
+
+    return revStr;
 }
 
 module.exports = reverseString
