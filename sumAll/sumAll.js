6cd2bb696536cc441b3d9f99bf6b2b6392be3352
Tanishq K <kronorak@outlook.com>
2021-02-22 21:38:22 +0530

    Update sumAll.js, sumAll.spec.js
       - Implemented the logic in sumAll.js
       - Enabled the temporarily disabled tests in sumAll.spec.js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    }
 
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if(num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    sum = (num2 * (num2 + 1) / 2) - ((num1 - 1) * num1 / 2);
+
+    return sum;
 }
 
 module.exports = sumAll
