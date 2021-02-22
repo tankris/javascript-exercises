37fd03439667ab44acff1abb2b89ea56db31cbb2
Tanishq K <kronorak@outlook.com>
2021-02-22 21:42:50 +0530

    Update leapYears.js, leapYears.spec.js
       - Implemented the logic in leapYears.js
       - Enabled the temporarily disabled tests in leapYears.spec.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // if year is divisible by 4 and 
+    // could be divisible by 400 and not divisible by 100
+    if(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    }
 
+    return false;
 }
 
 module.exports = leapYears
