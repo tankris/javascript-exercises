198d9a224e995116cc4a72e45e963826a14dec11
Tanishq K <kronorak@outlook.com>
2021-02-22 22:14:17 +0530

    Update tempConversion.js, tempCoversion.spec.js
       - Implemented the logic in tempCoversion.js
       - Enabled the temporarily disabled tests in tempConversion.spec.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 5/9;
+  return Math.round(temp * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (9/5 * temp) + 32;
+  return Math.round(temp * 10) / 10;
 }
 
 module.exports = {
